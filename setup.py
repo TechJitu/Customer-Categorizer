6ff2d2d5a8af7c1e9ee0acd73a4cfcb1c091f7a9
Jitendra Choudhary <jitendrachoudhary24007@gmail.com>
2026-08-01 07:55:05 +0530

    just a minor chanange in setup.py the nne has just change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="src",
+    name="Customer-Categorizer",
     version="0.0.1",
     author="iNeuron",
     author_email="cloud@ineuron.ai",
