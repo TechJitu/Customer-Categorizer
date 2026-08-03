ee4095895de3b34e507a679ddc695396d82da385
Jitendra Choudhary <jitendrachoudhary24007@gmail.com>
2026-08-03 10:51:10 +0530

    main_utilis file done

--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -168,3 +168,104 @@ class MainUtils:
         except Exception as e:
             raise CustomerException(e, sys) from e
 
+    @staticmethod
+    def save_object(file_path: str, obj: object) -> None:
+        logging.info("Entered the save_object method of MainUtils class")
+
+        try:
+            with open(file_path, "wb") as file_obj:
+                pickle.dump(obj, file_obj)
+
+            logging.info("Exited the save_object method of MainUtils class")
+
+        except Exception as e:
+            raise CustomerException(e, sys) from e
+
+    @staticmethod
+    def get_best_model_with_name_and_score(model_list: list) -> Tuple[object, float]:
+        logging.info(
+            "Entered the get_best_model_with_name_and_score method of MainUtils class"
+        )
+
+        try:
+            best_score = max(model_list)[0]
+
+            best_model = max(model_list)[1]
+
+            logging.info(
+                "Exited the get_best_model_with_name_and_score method of MainUtils class"
+            )
+
+            return best_model, best_score
+
+        except Exception as e:
+            raise CustomerException(e, sys) from e
+
+    @staticmethod
+    def load_object(file_path: str) -> object:
+        logging.info("Entered the load_object method of MainUtils class")
+
+        try:
+            with open(file_path, "rb") as file_obj:
+                obj = pickle.load(file_obj)
+
+            logging.info("Exited the load_object method of MainUtils class")
+
+            return obj
+
+        except Exception as e:
+            raise CustomerException(e, sys) from e
+
+    @staticmethod
+    def create_artifacts_zip(file_name: str, folder_name: str) -> None:
+        logging.info("Entered the create_artifacts_zip method of MainUtils class")
+
+        try:
+            shutil.make_archive(file_name, "zip", folder_name)
+
+            logging.info("Exited the create_artifacts_zip method of MainUtils class")
+
+        except Exception as e:
+            raise CustomerException(e, sys) from e
+
+    @staticmethod
+    def unzip_file(filename: str, folder_name: str) -> None:
+        logging.info("Entered the unzip_file method of MainUtils class")
+
+        try:
+            shutil.unpack_archive(filename, folder_name)
+
+            logging.info("Exited the unzip_file method of MainUtils class")
+
+        except Exception as e:
+            raise CustomerException(e, sys) from e
+
+    def update_model_score(self, best_model_score: float) -> None:
+        logging.info("Entered the update_model_score method of MainUtils class")
+
+        try:
+            model_config = self.read_model_config_file()
+
+            model_config["base_model_score"] = str(best_model_score)
+
+            with open(MODEL_TRAINER_MODEL_CONFIG_FILE_PATH, "w+") as fp:
+                safe_dump(model_config, fp, sort_keys=False)
+
+            logging.info("Exited the update_model_score method of MainUtils class")
+
+        except Exception as e:
+            raise CustomerException(e, sys) from e
+        
+    def save_numpy_array_data(self,file_path: str, array: np.array):
+        """
+        Save numpy array data to file
+        file_path: str location of file to save
+        array: np.array data to save
+        """
+        try:
+            dir_path = os.path.dirname(file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            with open(file_path, 'wb') as file_obj:
+                np.save(file_obj, array)
+        except Exception as e:
+            raise CustomerException(e, sys) from e
